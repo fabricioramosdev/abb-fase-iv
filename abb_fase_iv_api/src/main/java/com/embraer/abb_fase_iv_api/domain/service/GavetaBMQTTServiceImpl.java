bd5911be8f6611b1cd76b7cdb825d1c583cefc92
waldyr <waldyr.turquetti@escala-app.com>
2022-05-10 08:57:26 -0300

    feat: Add as interfaces Service e as classes impl.

---
+++ abb_fase_iv_api/src/main/java/com/embraer/abb_fase_iv_api/domain/service/GavetaBMQTTServiceImpl.java
@@ -0,0 +1,5 @@
+package com.embraer.abb_fase_iv_api.domain.service;
+
+public class GavetaBMQTTServiceImpl {
+}
+
---
+++ abb_fase_iv_api/src/main/java/com/embraer/abb_fase_iv_api/domain/service/GrupoColetaServiceImpl.java
@@ -0,0 +1,4 @@
+package com.embraer.abb_fase_iv_api.domain.service;
+
+public class GrupoColetaServiceImpl {
+}
